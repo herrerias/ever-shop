894a299eec2f8898afc9708c7389afb9b4ab9c2a
Juan Antonio Herrerias Berbel <jherreri@everis.com>
2018-07-24 13:04:12 +0200

    Mocks & Models

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from './item.model';
+import { ITEMS } from './mocks';
 
 @Component({
   selector: 'app-item-list',
@@ -6,39 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-list.component.css']
 })
 export class ItemListComponent implements OnInit {
+  myItems: Item[];
   
-  myItems = [{
-    'id': 1,
-    'name': 'Item name',
-    'description': 'These item is the best one',
-    'price': 14.99,
-    'stock': 5
-  },
-  {
-    'id': 2,
-    'name': 'Another Item name',
-    'description': 'These item is the smallest',
-    'price': 5,
-    'stock': 7
-  },
-  {
-    'id': 3,
-    'name': 'A cheap Item',
-    'description': 'The cheapest item!',
-    'price': 3.5,
-    'stock': 0}];
-
   constructor() { }
 
   ngOnInit() {
+    this.myItems = ITEMS;
   }
 
   totalItems() {
-    /*let sum = 0;
-    for (let item of this.myItems) {
-      sum += item.stock;
-    }
-    return sum;*/
     return this.myItems.reduce( (prev, current) => prev + current.stock, 0);
   }
 
---
+++ src/app/item-list/mocks.ts
@@ -0,0 +1,24 @@
+import { Item } from "./item.model";
+
+export const ITEMS: Item[] = [{
+    'id': 1,
+    'name': 'Item name',
+    'description': 'These item is the best one',
+    'price': 14.99,
+    'stock': 5
+  },
+  {
+    'id': 2,
+    'name': 'Another Item name',
+    'description': 'These item is the smallest',
+    'price': 5,
+    'stock': 7
+  },
+  {
+    'id': 3,
+    'name': 'A cheap Item',
+    'description': 'The cheapest item!',
+    'price': 3.5,
+    'stock': 0}];
+
+  
